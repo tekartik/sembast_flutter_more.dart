9f2f60b4e0fd74a2fcf3fa4540efa1265b3f5ac0
alex <alex@tekartik.com>
2020-08-07 14:44:30 +0200

    test: test firestore timestamp ordering

--- sembast_flutter/test/io_factory_test.dart
+++ sembast_flutter/test/io_factory_test.dart
@@ -1,14 +1,26 @@
 @TestOn('vm')
 library sembast_flutter.test.io_factory_test;
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast_test/all_fs_test.dart' as all_fs_test;
 import 'package:sembast_test/all_test.dart';
 import 'package:sembast_test/io_test_common.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sembast_test/all_fs_test.dart' as all_fs_test;
 
 void main() {
   var context = databaseContextIo;
   all_fs_test.defineTests(context);
   defineTests(databaseContextIo);
+
+  test('add', () async {
+    var store = intMapStoreFactory.store();
+    var db = await databaseContextIo.open('.dart_tool/test.db');
+    await store.drop(db);
+    await store.add(
+      db,
+      {'test': 'value'},
+    );
+    expect(
+        (await store.query().getSnapshots(db)).first.value, {'test': 'value'});
+  });
 }
